7fdfeebee8ce2f9f1757b56056ed88c9cd2f9d72
Milan Cerovsky <milan.cerovsky@inloop.eu>
2017-02-14 14:10:34 +0100

    Library documentation

--- library/src/main/java/eu/nerevar/shapeshifter/core/BackwardMode.java
+++ library/src/main/java/eu/nerevar/shapeshifter/core/BackwardMode.java
@@ -5,8 +5,19 @@ package eu.nerevar.shapeshifter.core;
  */
 public enum BackwardMode {
 
+    /**
+     * Pop backstack by one
+     */
     POP,
+
+    /**
+     * Pop all fragment transaction from backstack
+     */
     POP_WHOLE,
+
+    /**
+     * Pop all fragment transactions from backstack till we the fragment with given rood
+     */
     FRAGMENT_ROOT
 
 }
--- library/src/main/java/eu/nerevar/shapeshifter/core/ForwardMode.java
+++ library/src/main/java/eu/nerevar/shapeshifter/core/ForwardMode.java
@@ -1,13 +1,32 @@
 package eu.nerevar.shapeshifter.core;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Default backward navigation mode for {@link BaseNavigationController} built by a {@link ForwardBuilder}.
+ *
+ * TODO rethink the names of these states since they are not too convenient IMHO
  */
 public enum ForwardMode {
 
+    /**
+     * Add fragment to container over the current fragment using {@link android.support.v4.app.FragmentTransaction#add(int, Fragment, String)}
+     */
     ADDITION,
+
+    /**
+     * Replace fragment in container using {@link android.support.v4.app.FragmentTransaction#replace(int, Fragment, String)} without adding to backstack
+     */
     WITHOUT_REPLACEMENT,
+
+    /**
+     * Replace fragment in container using {@link android.support.v4.app.FragmentTransaction#replace(int, Fragment, String)} with adding to backstack
+     */
     REPLACEMENT,
+
+    /**
+     * Pop all fragments and add new fragment using {@link ForwardMode#WITHOUT_REPLACEMENT} method
+     */
     NEW
 
 }
